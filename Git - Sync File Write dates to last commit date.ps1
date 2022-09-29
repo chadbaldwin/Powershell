9338cb6aeb88345d03f914b1f971398b8a72daeb
Chad Baldwin <chad@chadbaldwin.net>
2022-09-29 15:05:27 -0400

    fix formatting

--- Git - Sync File Write dates to last commit date.ps1
+++ Git - Sync File Write dates to last commit date.ps1
@@ -1,6 +1,6 @@
 gci -Recurse -File |
     % {
-		$_.FullName;
-		$fileDate = git log -1 --pretty="format:%cI" $_.FullName;
-		$_.LastWriteTime = [datetime]$fileDate;
-	}
+        $_.FullName;
+        $fileDate = git log -1 --pretty="format:%cI" $_.FullName;
+        $_.LastWriteTime = [datetime]$fileDate;
+    }
